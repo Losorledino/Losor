1adf80c668474a876def85c223f97dae6c27f4a6
Losorledino <nicolaslouis1309@gmail.com>
2024-11-03 17:20:00 +0100

    carac9

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ new Typewriter(txtAnim, {
 .pauseFor(100)
 .deleteChars(10)
 .typeString('<span style="color: gray"> + windows</span> !')
-.deleteChars(30)
+.deleteChars(32)
 .typeString('<span style="color: lightblue"> étudiant BTS SIO</span> !')
 .start()
 
